fb5872121f730cdc75967bcd7adf5b57e5f3c16f
Zhangsnke <zhangxike@wondersgroup.com>
2018-11-29 17:14:40 +0800

    补充for循环

--- page/page1/info/info.js
+++ page/page1/info/info.js
@@ -1,9 +1,25 @@
 Page({
     data:{
+        id:0,
         msg:'信息数据',
         aflag:true,
         staffA:{name:'张三',age:'18'},
-        staffB:{name:"李四",age:'19'}
+        staffB:{name:"李四",age:'19'},
+        a:5,
+        b:6,
+        c:7,
+        d:1,
+        array:[{
+            message:'消息1'
+        },{
+            message:'消息2'
+        }],
+        objectArray: [
+            {id: 2, unique: 'unique_2'},
+            {id: 1, unique: 'unique_1'},
+            {id: 0, unique: 'unique_0'},
+          ],
+          numberArray: [1, 2, 3]
     },
    
     onLoad:function(options){
@@ -50,5 +66,31 @@ Page({
           desc: 'desc', // 分享描述
           path: 'path' // 分享路径
         }
-    }
+    },
+    switch: function(e) {
+        const length = this.data.objectArray.length
+        for (let i = 0; i < length; ++i) {
+          const x = Math.floor(Math.random() * length)
+          const y = Math.floor(Math.random() * length)
+          const temp = this.data.objectArray[x]
+          this.data.objectArray[x] = this.data.objectArray[y]
+          this.data.objectArray[y] = temp
+        }
+        this.setData({
+          objectArray: this.data.objectArray
+        })
+      },
+      addToFront: function(e) {
+        const length = this.data.objectArray.length
+        this.data.objectArray = [{id: length, unique: 'unique_' + length}].concat(this.data.objectArray)
+        this.setData({
+          objectArray: this.data.objectArray
+        })
+      },
+      addNumberToFront: function(e){
+        this.data.numberArray = [ this.data.numberArray.length + 1 ].concat(this.data.numberArray)
+        this.setData({
+          numberArray: this.data.numberArray
+        })
+      }
 })
